25921cdde15252dc1d96f4ca2badb210e0e8018e
James Fairless <xaghonova@gmail.com>
2025-03-05 23:06:55 -0600

    Full implement of MiddleEarthCouncil Singleton

---
+++ src/org/mizzoucs3330team/assignment_02/MiddleEarthCouncil.java
@@ -0,0 +1,29 @@
+package org.mizzoucs3330team.assignment_02;
+
+public class MiddleEarthCouncil {
+	private static MiddleEarthCouncil instance = new MiddleEarthCouncil();
+	private static CharacterManager cm = new CharacterManager();
+	
+	private MiddleEarthCouncil() {}
+	
+	public static MiddleEarthCouncil getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new MiddleEarthCouncil();
+		}
+		
+		return instance;
+	}
+	
+	public static CharacterManager getCharacterManger()
+	{
+		if(cm == null)
+		{
+			cm = new CharacterManager();
+		}
+		
+		return cm;
+	}
+
+}
